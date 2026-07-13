095ebd5000b10377cb71e7b97476aabcae3ce7f3
Hani Ali <YOUR_GITHUB_USERNAME@users.noreply.github.com>
2026-07-13 15:07:39 +0100

    Use non-manifest static storage on Heroku

--- config/settings.py
+++ config/settings.py
@@ -178,7 +178,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
